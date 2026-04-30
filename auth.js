330943a8d261ab9fd3d1be4b11c6bd47a665f19b
sanju-zxt <ssanju200610@gmail.com>
2026-05-01 00:13:39 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,29 +1,26 @@
-// paste your config here
 const firebaseConfig = {
-  apiKey: "PASTE_FROM_FIREBASE",
-  authDomain: "PASTE",
-  projectId: "PASTE"
+  apiKey: "AIzaSyBvd-t8jfrBRpee87Ha_7M628B5k2XsHYA",
+  authDomain: "smart-dashboard-9bc0a.firebaseapp.com",
+  projectId: "smart-dashboard-9bc0a",
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
-// Signup
 function signup() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   auth.createUserWithEmailAndPassword(email, password)
-    .then(() => alert("Signup successful"))
+    .then(() => alert("Signup success"))
     .catch(err => alert(err.message));
 }
 
-// Login
 function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   auth.signInWithEmailAndPassword(email, password)
-    .then(() => alert("Login successful"))
+    .then(() => window.location.href = "dashboard.html")
     .catch(err => alert(err.message));
 }
